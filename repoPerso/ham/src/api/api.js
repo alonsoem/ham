bd6113ececf10df2693a450ab61c7342d0244009
Enrique Alonso <alonso.em@gmail.com>
2022-12-21 19:58:14 -0300

    adding and removing some files

--- repoPerso/ham/src/api/api.js
+++ repoPerso/ham/src/api/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 
 const foreignRequest = (type, fullPath,params,config) => axios
-    .request({ url: `${fullPath}`, method: type, params:params, headers:{'Accept': 'application/xml'}})
+    .request({ url: `${fullPath}`, method: type, params:params, headers:config})
     .then(req => req.data);
 
-export const getSolar = (params) => foreignRequest('get', 'http://ham.qrits.com.ar/solarConditions/solarData.php',{},{});
+export const getSolar = (params) => foreignRequest('get', 'http://ham.qrits.com.ar/solarConditions/solarData.php',{},{'Content-Type':'application/json'});
 
 
--- repoPerso/ham/src/rootPanel.js
+++ repoPerso/ham/src/rootPanel.js
@@ -1,178 +1,21 @@
-import React from 'react';
-import {getSolar} from "./api/api";
-import {Card,Table} from "react-bootstrap";
+import React from "react";
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import solarConditions from "./solarConditions.js";
+
 import '../node_modules/bootstrap-css-only/css/bootstrap.css';
-import * as json from "fxparser";
 
 
-
-export default class RootPanel extends  React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            solarData:[],
-
-        };
-    }
-
-
-    update = () => {
-        getSolar({})
-            .then((data) => {
-                const { XMLParser } = require("fast-xml-parser");
-
-                const options = {
-                    ignoreAttributes: false,
-                    attributeNamePrefix : "@_",
-                    allowBooleanAttributes: true
-                };
-                const parser = new XMLParser(options);
-
-
-                let jObj = parser.parse(data);
-                console.log(json.parse(jObj))
-                this.setState({solarData: json.parse(jObj).solar.calculatedConditions});
-
-                //this.setState({solarData: data.solar});
-
-                }
-            )
-            .catch(() => this.setState({error: 'Algo anduvo mal! Volvé a internar'}));
-    }
-
-    componentDidMount() {
-
-        this.update();
-
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log (this.state.solarData);
-}
-
-
+export default class App extends React.Component {
     render() {
-
-        return (
-            <div className="container-fluid table-scroll-vertical">
-                <div className="row">
-                    <div className="col col-6 col-sm-12 col-md-6 col-xl-5">
-                <Card>
-                    <Card.Header>HF Band Info</Card.Header>
-
-                    <Card.Body>
-                <Table striped  hover >
-                    <thead>
-                    <tr>
-                        <th>Band</th>
-                        <th>Day</th>
-                        <th>Night</th>
-                    </tr>
-                    </thead>
-                    <tbody >
-
-
-                    {this.state.solarData.map(each=> (
-                        <tr>
-                            <td>NU</td>
-                            <td>NU</td>
-                            <td>NU</td>
-                        </tr>
-
-                    ))}
-
-
-                        <tr >
-                            <td>80-40</td>
-                            <td>Mal</td>
-                            <td>OK</td>
-                        </tr>
-                        <tr >
-                            <td>80-40</td>
-                            <td>Mal</td>
-                            <td>OK</td>
-                        </tr><tr >
-                            <td>80-40</td>
-                            <td>Mal</td>
-                            <td>OK</td>
-                        </tr><tr >
-                            <td>80-40</td>
-                            <td>Mal</td>
-                            <td>OK</td>
-                        </tr>
-
-
-
-                    </tbody>
-                </Table>
-                    </Card.Body>
-                </Card>
-
-                    </div></div>
-
-                <div className="row">
-                    <div className="col col-6 col-sm-12 col-md-6 col-xl-5">
-
-                <Card>
-                    <Card.Header>Solar Predictions</Card.Header>
-
-                    <Card.Body>
-                            [GRAFICA]
-
-                    </Card.Body>
-                </Card>
-                    </div></div>
-                    <div className="row">
-                        <div className="col col-6 col-sm-12 col-md-6 col-xl-5">
-                <Card>
-                    <Card.Header>Other solar Conditions</Card.Header>
-                    <Card.Body>
-
-                        <Table striped  hover >
-                            <thead>
-                            <tr>
-                                <th>Band</th>
-                                <th>Day</th>
-                                <th>Night</th>
-                            </tr>
-                            </thead>
-                            <tbody >
-
-
-                            <tr >
-                                <td>80-40</td>
-                                <td>Mal</td>
-                                <td>OK</td>
-                            </tr>
-                            <tr >
-                                <td>80-40</td>
-                                <td>Mal</td>
-                                <td>OK</td>
-                            </tr><tr >
-                                <td>80-40</td>
-                                <td>Mal</td>
-                                <td>OK</td>
-                            </tr><tr >
-                                <td>80-40</td>
-                                <td>Mal</td>
-                                <td>OK</td>
-                            </tr>
-
-
-
-                            </tbody>
-                        </Table>
-
-                    </Card.Body>
-                </Card>
-                        </div></div>
-
-            </div>
-
-
-        );
-
-
+      return (
+          <div   >
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={solarConditions} />
+          </Switch>
+        </BrowserRouter>
+          </div>
+      );
     }
-
-}
+  }
