669483d8513999b27913cbb889753c3766681b15
Enrique Alonso <alonso.em@gmail.com>
2022-12-27 13:23:20 -0300

    Fix warnings for CI

--- src/updates.js
+++ src/updates.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import {Card,Table} from "react-bootstrap";
 import '../node_modules/bootstrap-css-only/css/bootstrap.css';
 
 
@@ -24,7 +22,7 @@ export default class updates extends  React.Component {
       
 
         return (
-            <div className="container-fluid table-scroll-vertical">
+            <div className="container-fluid table-scroll-vertical bgdiv">
                     <h1>&Uacute;ltimas actualizaciones</h1>
         <p>Esta aplicaci&oacute;n muestra informaci&oacute;n sobre licencias de radioaficionados de distintos paises. En cada pa&iacute;s un ente oficial se encarga de llevar el registro y nosotros usamos esos registros para mostrarlos en este servicio.<br/>
         Aqu&iacute; mostramos cuando fue la &uacute;ltima vez que se publicaron los datos que aqui les ofrecemos.
@@ -42,7 +40,7 @@ export default class updates extends  React.Component {
   </thead>
   <tbody>
     <tr>
-      <th scope="row"><img src="static/flags/gif/ar.gif" /></th>
+      <th scope="row"><img src="static/flags/gif/ar.gif" alt="Argentina flag" /></th>
       <td>Argentina</td>
       <td>07-11-2022</td>
       <td><a href="https://www.enacom.gob.ar/listado-de-radioaficionados_p316">
@@ -52,7 +50,7 @@ export default class updates extends  React.Component {
     
     </tr>
     <tr>
-      <th scope="row"><img src="static/flags/gif/br.gif" /></th>
+      <th scope="row"><img src="static/flags/gif/br.gif"  alt="Brasil flag"/></th>
       <td>Brasil</td>
       <td>03-10-2022</td>
       <td><a href="https://www.anatel.gov.br/dadosabertos/paineis_de_dados/outorga_e_licenciamento/estacoes_licenciadas.zip">
@@ -61,7 +59,7 @@ export default class updates extends  React.Component {
       </td>
     </tr>
     <tr>
-      <th scope="row"><img src="static/flags/gif/cl.gif" /></th>
+      <th scope="row"><img src="static/flags/gif/cl.gif" alt="Chile flag" /></th>
       <td>Chile</td>
       <td>25-11-2022</td>
       <td><a href="https://www.subtel.gob.cl/inicio-concesionario/servicios-de-telecomunicaciones/servicios-de-radio-aficionados/">
@@ -70,7 +68,7 @@ export default class updates extends  React.Component {
       </td>
     </tr>
     <tr>
-      <th scope="row"><img src="static/flags/gif/uy.gif" /></th>
+      <th scope="row"><img src="static/flags/gif/uy.gif" alt="Uruguay flag"/></th>
       <td>Uruguay</td>
       <td>03-11-2022</td>
       <td><a href="https://www.gub.uy/unidad-reguladora-servicios-comunicaciones/datos-y-estadisticas/datos/registro-radioaficionados">
@@ -80,7 +78,7 @@ export default class updates extends  React.Component {
       
     </tr>
         <tr>
-      <th scope="row"><img src="static/flags/gif/pe.gif" /></th>
+      <th scope="row"><img src="static/flags/gif/pe.gif" alt="Perú flag"/></th>
       <td>Per&uacute;</td>
       <td>11-11-2022</td>
       <td><a href="https://www.gob.pe/institucion/mtc/informes-publicaciones/396528-radioaficionados-con-autorizacion-vigente-direccion-de-servicios-en-telecomunicaciones-dgat">
