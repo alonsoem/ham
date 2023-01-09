74552405b5a1950301dd973387cd3b683ee4a3bd
Enrique Alonso <alonso.em@gmail.com>
2023-01-09 10:27:17 -0300

    Internacionalizacion y resultados 2

---
+++ App18.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import i18n from './src/i18n';
+import { withTranslation } from 'react-i18next';
+
+
+function App18 ({ t }) {
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        sessionStorage.setItem("language",lng);
+
+    }
+
+    const handleChange = (event) => {
+        changeLanguage(event.target.value);
+    };
+
+    return (
+                <select className="btn btn-outline-info form-select ms-0"
+                        onChange={handleChange}
+                        defaultValue={sessionStorage.getItem("language")}
+                        name="language"
+                        id={"language_selector"}
+                >
+                    <option value={'es'}>Español</option>
+                    <option value={'en'}>English</option>
+                </select>
+    )
+}
+
+export default withTranslation()(App18);
