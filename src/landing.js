df7678417a15567596d742b98fb75802e3d6ee1c
Enrique Alonso <alonso.em@gmail.com>
2023-01-25 17:07:25 -0300

    eslint fixes

--- src/landing.js
+++ src/landing.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { Link ,Button} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import '../node_modules/bootstrap-css-only/css/bootstrap.css';
 
