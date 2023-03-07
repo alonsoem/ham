4e8f06a9b782322ea5a383d5225b8bb7b1ca97d3
Enrique Alonso <alonso.em@gmail.com>
2023-03-07 13:29:13 -0300

    fix tools view

--- src/tools.js
+++ src/tools.js
@@ -22,205 +22,167 @@ export default class tools extends  React.Component {
               <div class="card-header bgdiv text-white">
                 <h1>Herramientas</h1> 
             </div>
+
+
             <p>&nbsp;</p>
                      
 
-
-    
-
-
-
+            
 
             <div className="container">
-     <div className="card-columns">
 
-     <div class="card text-center col-5">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
+           
 
-     <div class="card text-center col-5">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
-     <div class="card text-center col-5">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
-     <div class="card text-center col-5">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
-     <div class="card text-center">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
-     <div class="card text-center">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
-     <div class="card text-center">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
-     <div class="card text-center">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
+            <div class="row mb-3">
 
+              <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Código Fonetico</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>
 
+              
+              <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Sufijos Argentina</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=sufijos-argentina.webp" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>
 
-<div class="row col-xs-12">
-    <div class="col-xs-3">
-      <div class="card w-100 text-center">
-      <div class="card-body">
-        <p>Codigo Fonetico</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigo-ICAO.png" class="btn btn-primary">Ir</a>
-      </div>
-     </div>   
-    </div>
-    
-    <div class="col-xs-3">
-     <div class="card w-100 text-center">
-      <div class="card-body">
-        <p>Sufijos Argentina</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=sufijos-argentina.webp" class="btn btn-primary">Ir</a>
-      </div>
-     </div>
-    </div>      
-  
-    <div class="col-xs-3">
-     <div class="card w-100 text-center">
-      <div class="card-body">
-        <p>Sufijos Uruguay</p>
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=sufijos-uruguay.gif" class="btn btn-primary">Ir</a>
-      </div>
-    </div>
-  </div>      
-  
-</div>
-
-       <div class="row">
-  <div class="col-xs-3">
-      <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Prefijos Chile</p>
-        
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=prefijos-chile.jpg" class="btn btn-primary">Ir</a>
-      </div>
-    </div>   
-    </div>
-    <div class="col-xs-3">
-     <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Prefijos Brasil</p>
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=prefijos-brasil.jpg" class="btn btn-primary">Ir</a>
-      </div>
-    </div>
-  
-  </div>      
-  
-    <div class="col-xs-3">
-     <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Codigo Morse</p>
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=morse.jpg" class="btn btn-primary">Ir</a>
-      </div>
-    </div>
-  
-  </div>      
-  </div>
-  
-       <div class="row">
-  <div class="col-xs-3">
-      <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Código RST</p>
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=rst.jpg" class="btn btn-primary">Ir</a>
-      </div>
-    </div>   
-    </div>
-    <div class="col-xs-3">
-     <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Código Q</p>
-        <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigoQ.jpg" class="btn btn-primary">Ir</a>
-      </div>
-    </div>
-  
-  </div>      
-  
-    <div class="col-xs-3">
-     <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Bandas y frecuencias</p>
-          <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=bandas.png" class="btn btn-primary">Ir</a>
-      </div>
-    </div>
-  
-  </div>      
-  </div>
-  
-         <div class="row">
-  <div class="col-xs-3">
-      <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Hora UTC</p>
-        <a href="/utc" class="btn btn-primary">Ir</a>
-      </div>
-    </div>   
-    </div>
-    <div class="col-xs-3">
-     <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Llamado CQ</p>
-        <a href="/cqcall" class="btn btn-primary">Ir</a>
-      </div>
-    </div>
-  
-  </div>      
-  
-    <div class="col-xs-3">
-     <div class="card w-100 text-center">
-      <div class="card-body">
-        <p class="card-title">Condiciones Solares</p>
-        <a href="/solarConditions" class="btn btn-primary">Ir</a>
-      </div>
-    </div>
-  
-  </div>      
-  </div>
-          
-          
-  </div>
-  </div>
+              <div class="col-4 col-sm-4 ">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Sufijos Uruguay</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=sufijos-uruguay.gif" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>  
+   
             </div>
+
+
+            
+            <div class="row mb-3">
+
+            <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Prefijos Chile</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=prefijos-chile.jpg" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>
+
+              
+              <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Prefijos Brasil</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=prefijos-brasil.jpg" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>
+
+              <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Código Morse</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=morse.jpg" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>  
+   
+            </div>
+
+
+            
+            <div class="row mb-3">
+
+            <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Código RST</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=rst.jpg" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>
+
+              
+              <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Código Q</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=codigoQ.jpg" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>
+
+              <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Bandas y frecuencias</p>
+                    <a href="http://ham.qrits.com.ar/app-pages/viewer.php?img=bandas.png" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>  
+   
+            </div>
+
+
+            
+            <div class="row mb-3">
+
+            <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Hora UTC</p>
+                    <a href="http://ham.qrits.com.ar/utc" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>
+
+              
+              <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Llamado CQ</p>
+                    <a href="http://ham.qrits.com.ar/cqcall" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>
+
+              <div class="col-4 col-sm-4">
+                <div class="card text-center">
+                  <div class="card-body">
+                    <p class="fs-1 text-uppercase">Condiciones solares</p>
+                    <a href="http://ham.qrits.com.ar/solarConditions" class="btn btn-primary">Ir</a>
+                  </div>
+                </div>   
+              </div>  
+   
+            </div>
+
+
+
+            
+
+
+
+
+
+
+            </div>
+
+
+
+
+
+  </div>
 
 
         );
