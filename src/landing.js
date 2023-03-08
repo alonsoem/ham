1ffb423611bc6bda1fb40d58226f3e1d7b15389a
Enrique Alonso <alonso.em@gmail.com>
2023-03-08 16:14:05 -0300

    fix center navbar

--- src/landing.js
+++ src/landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+//import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../node_modules/bootstrap-css-only/css/bootstrap.css';
 
