40872db95d6927a4090855b3923b610f0a925657
Enrique Alonso <alonso.em@gmail.com>
2023-05-31 14:38:15 -0300

    fix cards style

--- src/feedback.js
+++ src/feedback.js
@@ -18,17 +18,14 @@ export default class landing extends  React.Component {
         };
     }
     
-     handleChange = (event) => {
-         
+    handleChange = (event) => {
         this.setState({comment:event.target.value});
-      };
+    };
 
 
-      handleSubmit = (event) => {
-        console.log("envio!");
-        console.log (this.state.comment);
+    handleSubmit = (event) => {
         postFeedback({comments:this.state.comment});
-      };
+    };
 
     componentDidMount() {
                this.update();
@@ -38,15 +35,26 @@ export default class landing extends  React.Component {
     update = () => {
         getFeedback({})
             .then((data) => {
-            
-                this.setState({feedbacks:data.feedbacks});
-                     
+                    this.setState({feedbacks:data.feedbacks});
                 })
-             
-            
             .catch(() => this.setState({error: 'Algo anduvo mal! Volvé a intentar'}));
     }
+
+
     render() {
+
+        const showStatus= (status)=>{
+            switch(status) {
+                case "1":
+                  return "bg-light";
+                case "2":
+                    return "bg-success";
+                case "3":
+                    return "bg-secondary";
+                default:
+                    return "";
+              } 
+        }
         return (
 
             <div >
@@ -107,23 +115,22 @@ export default class landing extends  React.Component {
                     </div>
 
 
+                    <div>&nbsp;</div>
 
-                    <div>
-        
+                    <div className="container block">
+                    
                     {this.state.feedbacks.map((oneFeedback)=>(
-                    <div class="col-3 col-sm-3">
-                <div class={"card text-center "+(oneFeedback.status===1? "bg-info":"bg-success")}>
-                
-                  <div class="card-body">
+                    
+                        <div class={"card quique "+showStatus(oneFeedback.status)} >
+                            <div class="card-body text-left">
+                                <p class="fs-0 ">{oneFeedback.comment}</p>
+                            </div>
+                        </div>   
               
-                    <p class="fs-1 text-uppercase">{oneFeedback.comment}</p>
-                  </div>
-                </div>   
-              </div>
 
-
+                        
                     ))}
-
+                    
                     </div>
             </div>
             
