1a0a9fcde9709a355d1987792a0da87bdfb694f3
Enrique Alonso <alonso.em@gmail.com>
2024-01-17 10:03:38 -0300

    fix v2 url

--- src/tools.js
+++ src/tools.js
@@ -41,7 +41,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Código Fonético</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Código Fonético/codigo-ICAO.png" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Código Fonético/codigo-ICAO.png" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>
@@ -51,7 +51,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Sufijos Argentina</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Sufijos Argentina/sufijos-argentina.webp" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Sufijos Argentina/sufijos-argentina.webp" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>
@@ -66,7 +66,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Sufijos Uruguay</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Sufijos Uruguay/sufijos-uruguay.gif" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Sufijos Uruguay/sufijos-uruguay.gif" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>                
@@ -75,7 +75,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Prefijos Chile</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Prefijos Chile/prefijos-chile.jpg" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Prefijos Chile/prefijos-chile.jpg" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>
@@ -91,7 +91,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Prefijos Brasil</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Prefijos Brasil/prefijos-brasil.jpg" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Prefijos Brasil/prefijos-brasil.jpg" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>
@@ -100,7 +100,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Código Morse</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Código Morse/morse.jpg" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Código Morse/morse.jpg" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>  
@@ -113,7 +113,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Código RST</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Código RST/rst.jpg" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Código RST/rst.jpg" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>
@@ -123,7 +123,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Código Q</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Código Q/codigoQ.jpg" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Código Q/codigoQ.jpg" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>
@@ -136,7 +136,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Bandas y frecuencias</p>
-                    <a href="https://ham.qrits.com.ar/v2/viewer/Bandas y frecuencias/bandas.png" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/viewer/Bandas y frecuencias/bandas.png" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>                
@@ -145,7 +145,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Hora UTC</p>
-                    <a href="https://ham.qrits.com.ar/v2/utc" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/utc" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>
@@ -160,7 +160,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Llamado CQ</p>
-                    <a href="https://ham.qrits.com.ar/v2/cqcall" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/cqcall" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>
@@ -169,7 +169,7 @@ export default class tools extends  React.Component {
                 <div class="card text-center">
                   <div class="card-body">
                     <p class="fs-6 text-uppercase">Condiciones solares</p>
-                    <a href="https://ham.qrits.com.ar/v2/solarConditions" class="btn btn-primary">Ir</a>
+                    <a href="https://ham.qrits.com.ar/solarConditions" class="btn btn-primary">Ir</a>
                   </div>
                 </div>   
               </div>  
--- src/topMenu.js
+++ src/topMenu.js
@@ -29,34 +29,34 @@ const TopMenu = () => {
 
 
     <Navbar collapseOnSelect bg="light" className="navbar" expand="sm">
-          <Nav.Link href="/v2/">Búsqueda</Nav.Link>
-          <Nav.Link href="/v2/repeaters">Repetidoras</Nav.Link>        
+          <Nav.Link href="/">Búsqueda</Nav.Link>
+          <Nav.Link href="/repeaters">Repetidoras</Nav.Link>        
             
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="nav me-auto">
 
-             <NavDropdown title="Herramientas" id="nav-dropdown" href="/v2/tools" >
-               <NavDropdown.Item href="/v2/viewer/Bandas y frecuencias/bandas.png">Bandas y frecuencias</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/viewer/Código Fonético/codigo-ICAO.png" >Código Fonético</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/viewer/Código Morse/morse.jpg">Código Morse</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/viewer/Código RST/rst.jpg">Código RST</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/viewer/Código Q/codigoQ.jpg">Código Q</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/solarConditions">Condiciones Solares</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/utc">Hora UTC</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/viewer/Prefijos Chile/prefijos-chile.jpg">Prefijos Chile</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/viewer/Prefijos Brasil/prefijos-brasil.jpg">Prefijos Brasil</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/viewer/Sufijos Uruguay/sufijos-uruguay.gif">Sufijos Uruguay</NavDropdown.Item>
-                <NavDropdown.Item href="/v2/viewer/Sufijos Argentina/sufijos-argentina.webp">Sufijos Argentina</NavDropdown.Item>              
-                <NavDropdown.Item href="/v2/cqcall">Llamado CQ</NavDropdown.Item>
+             <NavDropdown title="Herramientas" id="nav-dropdown" href="/tools" >
+               <NavDropdown.Item href="/viewer/Bandas y frecuencias/bandas.png">Bandas y frecuencias</NavDropdown.Item>
+                <NavDropdown.Item href="/viewer/Código Fonético/codigo-ICAO.png" >Código Fonético</NavDropdown.Item>
+                <NavDropdown.Item href="/viewer/Código Morse/morse.jpg">Código Morse</NavDropdown.Item>
+                <NavDropdown.Item href="/viewer/Código RST/rst.jpg">Código RST</NavDropdown.Item>
+                <NavDropdown.Item href="/viewer/Código Q/codigoQ.jpg">Código Q</NavDropdown.Item>
+                <NavDropdown.Item href="/solarConditions">Condiciones Solares</NavDropdown.Item>
+                <NavDropdown.Item href="/utc">Hora UTC</NavDropdown.Item>
+                <NavDropdown.Item href="/viewer/Prefijos Chile/prefijos-chile.jpg">Prefijos Chile</NavDropdown.Item>
+                <NavDropdown.Item href="/viewer/Prefijos Brasil/prefijos-brasil.jpg">Prefijos Brasil</NavDropdown.Item>
+                <NavDropdown.Item href="/viewer/Sufijos Uruguay/sufijos-uruguay.gif">Sufijos Uruguay</NavDropdown.Item>
+                <NavDropdown.Item href="/viewer/Sufijos Argentina/sufijos-argentina.webp">Sufijos Argentina</NavDropdown.Item>              
+                <NavDropdown.Item href="/cqcall">Llamado CQ</NavDropdown.Item>
                 <NavDropdown.Item href="http://www.amsat.org.ar/pass.htm">Tracking satélites</NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="/v2/tools">Todo</NavDropdown.Item>
+                  <NavDropdown.Item href="/tools">Todo</NavDropdown.Item>
               </NavDropdown>
           
           
-              <Nav.Link href="/v2/updates">Actualizaciones</Nav.Link>
-              <Nav.Link href="/v2/feedback">Envía un comentario</Nav.Link>
+              <Nav.Link href="/updates">Actualizaciones</Nav.Link>
+              <Nav.Link href="/feedback">Envía un comentario</Nav.Link>
               
               
             
