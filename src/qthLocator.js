aaf94c1ecb44a16130c318fcdd7207cd12e28f58
Enrique Alonso <alonso.em@gmail.com>
2024-02-29 23:49:20 -0300

    first map interaction

--- src/qthLocator.js
+++ src/qthLocator.js
@@ -1,24 +1,65 @@
 import React, { useState } from 'react';
 import './index.css';
 import TopMenu from './topMenu';
-import { useEffect } from "react";
+import { useEffect,useRef} from "react";
 import { Marker,Popup,MapContainer,TileLayer } from 'react-leaflet';
+import { useMapEvent} from 'react-leaflet/hooks';
+
 
 
 export default function QthLocator (props) {
-    const [maidenhead, setMaiden] = useState("");
+    
 
-    const position = [-34.735875875962634, -58.24512750615829];
+    const [position,setPosition] = useState([-34.735875875962634, -58.24512750615829]);
+    const mapRef = useRef(null);
+    const markerRef = useRef(null);
+      
+      
+     
 
-    const handleOnChangeMaiden = (event) =>{
-      setMaiden(event.target.value);
-    }
+ 
+      function LocationMarker() {
+        const map = useMapEvent('click', (e) => {
+          setPosition([  e.latlng.lat,  e.latlng.lng]);
+          map.setView(e.latlng);
+          
+          openPopup();
+        })
 
-      useEffect(() => {
-          //updateBands();
+
+        useEffect(() => {
+          console.log(position);
+          openPopup();
         // eslint-disable-next-line
-      }, []);
+      }, [position]);
+        
 
+       
+      
+        return position === null ? null : (
+          <Marker position={position} ref={markerRef} >
+            <Popup>{position}</Popup>
+          </Marker>
+        )
+      }
+
+      function openPopup(){
+          const map = mapRef.current;
+          if (!map) {
+            return;
+          }
+
+          map.flyTo(position);
+
+          const marker = markerRef.current;
+          if (marker) {
+            marker.openPopup();
+          }
+      }
+
+
+       
+      
 
 
         return (
@@ -35,37 +76,29 @@ export default function QthLocator (props) {
                         <div style={{ position: 'fixed',width:'100%',top:'23%',zIndex:10000, margin:'auto'}} >
                           <div class="input-group m-2 w-50 ml-auto mr-auto">
                             <span class="input-group-text" id="inputGroup-sizing-default">UBICACIÓN</span>
-                            <input type="text" value={maidenhead} onChange={handleOnChangeMaiden} class="form-control mr-4 "  /> 
+                            <input type="text" value={position}  class="form-control mr-4 "  /> 
                           </div>
                         </div>
                         
 
  
-                        <MapContainer id="map" center={position} zoom={13}  zoomControl={false}>
+                        <MapContainer ref={mapRef} id="map" center={position} zoom={13}  zoomControl={false} >
                           <TileLayer
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                           />
-                          <Marker position={position}>
-                            <Popup>
-                              A pretty CSS3 popup. <br /> Easily customizable.
-                            </Popup>
-                          </Marker>
+                          
+                          
+                          <LocationMarker />
+                          
+                          
+        
                         </MapContainer>
 
                         
                       </div>
           </div>
 
-                
-        
-                
-                                                
-                                            
-                                   
+                       
         );
-
-
-    
-
 }
