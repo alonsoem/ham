f5009ad5f586173ffaedf3390b5088603b1c7afc
Enrique Alonso <alonso.em@gmail.com>
2024-03-05 17:12:15 -0300

    fix maiden

--- src/qthLocator.js
+++ src/qthLocator.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import './index.css';
 import TopMenu from './topMenu';
 import { useEffect,useRef} from "react";
-import { Marker,Popup,MapContainer,TileLayer } from 'react-leaflet';
-import { useMapEvent} from 'react-leaflet/hooks';
+import { Marker,Popup,MapContainer,TileLayer} from 'react-leaflet';
+import { useMapEvent,useMap} from 'react-leaflet/hooks';
+
 
 
 
@@ -11,13 +12,23 @@ export default function QthLocator (props) {
     
 
     const [position,setPosition] = useState([-34.735875875962634, -58.24512750615829]);
+    const [maidenPosition,setMaidenPosition] = useState("");
+    
     const mapRef = useRef(null);
     const markerRef = useRef(null);
       
+    const str_chr_up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";                          // Constants.
+    const str_chr_lo = "abcdefghijklmnopqrstuvwxyz";
+    const str_num = "0123456789";
       
      
-
+const handleChangeMaiden = (event) =>{
+  setMaidenPosition(event.target.value);
+  
+}
  
+
+
       function LocationMarker() {
         const map = useMapEvent('click', (e) => {
           setPosition([  e.latlng.lat,  e.latlng.lng]);
@@ -25,6 +36,7 @@ export default function QthLocator (props) {
           
           openPopup();
         })
+      }
 
 
         useEffect(() => {
@@ -36,30 +48,119 @@ export default function QthLocator (props) {
 
        
       
-        return position === null ? null : (
-          <Marker position={position} ref={markerRef} >
-            <Popup>{position}</Popup>
-          </Marker>
-        )
-      }
-
+     
       function openPopup(){
-          const map = mapRef.current;
-          if (!map) {
-            return;
-          }
+        const map = mapRef.current;
+        if (!map) {
+          return;
+        }
 
-          map.flyTo(position);
+        map.flyTo(position);
 
-          const marker = markerRef.current;
-          if (marker) {
-            marker.openPopup();
-          }
-      }
+        const marker = markerRef.current;
+        if (marker) {
+          marker.openPopup();
+        }
+    }
 
 
+    const gotoMaiden= ()=>{
+      setPosition(getPositionFromMaiden());
+    
+    }
+
+      const getPositionFromMaiden= ()=>{
+        var lat;
+        var lon;
+        const qth=maidenPosition;
+        console.log(qth);
+
+        lat = str_chr_up.indexOf(qth.charAt(1)) * 10;               // 2nd digit: 10deg latitude slot.
+        lon = str_chr_up.indexOf(qth.charAt(0)) * 20;               // 1st digit: 20deg longitude slot.
+        lat += str_num.indexOf(qth.charAt(3)) * 1;                  // 4th digit: 1deg latitude slot.
+        lon += str_num.indexOf(qth.charAt(2)) * 2;                  // 3rd digit: 2deg longitude slot.
+        console.log(lat);
+        console.log(lon);
+        if (qth.length == 6)
+        {
+            lat += str_chr_lo.indexOf(qth.charAt(5)) * 2.5 / 60;    // 6th digit: 2.5min latitude slot.
+            lon += str_chr_lo.indexOf(qth.charAt(4)) * 5 / 60;      // 5th digit: 5min longitude slot.
+        }
+
+        if (qth.length == 4)                                        // Get coordinates of the center of the square.
+        {
+            lat += 0.5 * 1;
+            lon += 0.5 * 2;
+        }
+        else
+        {
+            lat += 0.5 * 2.5 / 60;
+            lon += 0.5 * 5 / 60;
+        }
+
+        lat -= 90;                                                  // Locator lat/lon origin shift.
+        lon -= 180;
+        console.log(lat);
+        console.log(lon);
+
+        return ([lat,lon]);
+        
        
       
+      }
+
+
+      function MaidenPointer(){
+        const map = useMap();
+        map.setView(gotoMaiden());
+          
+          openPopup();
+      }
+      
+       
+      function maidenHead  () {
+        var lat =position[0];
+        var lon =position[1];
+        var qth;
+
+       
+
+
+        lat+= 90;
+                                                          // Locator lat/lon shift.
+        lon += 180;
+
+        qth = str_chr_up.charAt(Math.floor(lon / 20));              // 1st digit: 20deg longitude slot.
+        qth += str_chr_up.charAt(Math.floor(lat / 10));             // 2nd digit: 10deg latitude slot.
+        qth += str_num.charAt(Math.floor((lon % 20) / 2));          // 3rd digit: 2deg longitude slot.
+        qth += str_num.charAt(Math.floor((lat % 10) / 1));          // 4th digit: 1deg latitude slot.
+        qth += str_chr_lo.charAt(Math.floor((lon % 2) * (60 / 5))); // 5th digit: 5min longitude slot.
+        qth += str_chr_lo.charAt(Math.floor((lat % 1) * (60 / 2.5)));  // 6th digit: 2.5min latitude slot.
+        return qth;
+       }
+
+       
+
+       function MaidenPointer  (){
+       return position === null ? null : (
+        <Marker position={position} ref={markerRef} >
+          <Popup>
+              <div class="card">
+                <div className={"card-header"}>QTH locator</div>
+                <div class="card-body">
+                  <p>{maidenHead()}</p>
+                  <p>{"Latitud:  " +position[0]}</p>
+                  <p>{"Longitud: " +position[1]}</p>
+
+                </div>
+                
+              </div>
+              
+              
+          </Popup>
+        </Marker>
+      )
+    }
 
 
         return (
@@ -72,11 +173,15 @@ export default function QthLocator (props) {
                         <h1>Ubicación de QTH</h1> 
                       </div>
 
-                      <div className="container-fluid  m-0 p-0 ">
-                        <div style={{ position: 'fixed',width:'100%',top:'23%',zIndex:10000, margin:'auto'}} >
-                          <div class="input-group m-2 w-50 ml-auto mr-auto">
-                            <span class="input-group-text" id="inputGroup-sizing-default">UBICACIÓN</span>
-                            <input type="text" value={position}  class="form-control mr-4 "  /> 
+                      <div className="container-fluid  m-0 p-0 align-center">
+                        <div style={{ position: 'fixed',width:'100%',top:'20%',zIndex:10000, margin:'auto'}} >
+                          <div class="input-group  w-50 ml-auto mr-auto">
+                            
+                            
+                            <input type="text" value={maidenPosition}  class="form-control  " onChange={handleChangeMaiden}  /> 
+                              <div class="input-group-append">
+                                <button class="btn btn-outline-secondary bg-success" onClick={gotoMaiden} type="button">UBICAR</button>
+                              </div>
                           </div>
                         </div>
                         
@@ -90,7 +195,7 @@ export default function QthLocator (props) {
                           
                           
                           <LocationMarker />
-                          
+                          <MaidenPointer />
                           
         
                         </MapContainer>
